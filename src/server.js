324f58d4c735111006accc4ba4fb7cfa3d2c0b3f
Jack Frost <kodeandplay@gmail.com>
2016-12-16 22:20:41 -0800

    Added cheerio

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import socketio from 'socket.io';
 import axios from 'axios';
+import cheerio from 'cheerio';
 
 const app = express();
 
@@ -13,6 +14,7 @@ const server = app.listen(app.get('port'), () => {
 const io = socketio(server);
 
 const address = {};
+const country = {};
 
 const updateCount = (ip) => {
   let idx = ip.lastIndexOf(':');
@@ -21,13 +23,29 @@ const updateCount = (ip) => {
   getCountry(ip);
 }
 
+const getFlag = (cntry) => {
+  axios.get(`https://www.google.com/search?q=${cntry}+flag+image`)
+    .then(response => {
+      console.log('response:', response);
+      let $ = cheerio.load(response);
+      let image = $('#uid_0').attr('src');
+      console.log('image:', image);
+    })
+    .catch(error => {
+      console.log('error:', error);
+    });
+}
+
 const getCountry = (ip) => {
   axios.get(`http://ipinfo.io/${ip}/json`)
     .then(response => {
-      console.log("response:", response);
+      console.log('response.data:', response.data);
+      let cntry = response.data.country;
+      country[cntry] = (country[cntry] || 0) + 1;
+      getFlag(cntry);
     })
     .catch(error => {
-      console.log("error:", error);
+      console.log('error:', error);
     });
 }
 
