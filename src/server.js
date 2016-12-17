777bc8e0fa77390860366855ba37dacf350661e1
Jack Frost <kodeandplay@gmail.com>
2016-12-16 19:08:02 -0800

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import socketio from 'socket.io';
+
+const app = express();
+
+app.set('port', process.env.PORT || 3005);
+
+const server = app.listen(app.get('port'), () => {
+  console.log('Server listening on port', app.get('port'));
+});
+
+const io = socketio(server);
+
+app.get('/', (req, res) => {
+  console.log('remoteAddress:', req.connection.remoteAddress);
+  console.log('ip:', req.ip);
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('new connection');
+
+  socket.on('disconnect', () => {
+    console.log('connection closed');
+  });  
+
+});
